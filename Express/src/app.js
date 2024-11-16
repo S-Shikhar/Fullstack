59357fb9553b2dbf0009a3c9fa4f5e39a2970090
S-Shikhar <souhardashikharbiswas@gmail.com>
2024-11-16 22:43:39 +0530

    Set up Express application with view engine and static file serving; establish database connection and configure body parser

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,23 +1,34 @@
-function isPrime(num) {
-    for (var i = 2; i < num/2; i++) {
-        if (num % i == 0) return false;
-    }
-    return true;
-}
+// Importing the required modules
+const express =require("express");
+const hbs = require("hbs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const register = require("./models/register");
+const conn = require("./db/conn");
 
-peoples = [
-    ["Rouymadip Patra", 86],
-    ["Suvendu Dash Adhikary", 117],
-    ["Souharda Shikhar Biswas", 103]
-]
+// Creating an express app
+const app = express();
+const port = 3400 || process.env.PORT;
 
-for (let people of peoples) {
-    for (let i of people) {
-        console.log(i)
-    }
-}
+// Database connection
+conn();
 
-for (let [name, roll] of peoples) {
-    console.log(`Name: ${name} and Roll:${roll}`);
-}
+// Setting the view engine
+app.set("view engine", "hbs");
+
+// Setting the path for views
+app.set("views", path.join(__dirname, "../templates/views"));
+
+// Setting the path for partials
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+
+// Setting the path for static files
+app.use("/css", express.static(path.join(__dirname, "../templates/assets/css")));
+app.use("/js", express.static(path.join(__dirname, "../templates/assets/js")));
+app.use("/img", express.static(path.join(__dirname, "../templates/assets/img")));
+
+// Parsing the data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 

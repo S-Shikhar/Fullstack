92308ddea3f3b2a04f3611ef4c5112119caceaec
S-Shikhar <souhardashikharbiswas@gmail.com>
2024-11-16 22:22:32 +0530

    Implement prime number checker and add student details display; create initial Express project structure

--- Test JS/test.js
+++ Test JS/test.js
@@ -20,3 +20,27 @@ test(2, 5.5);
 var d = (a, b) => {
     return a + b;
 }
+
+function isPrime(num) {
+    for (var i = 2; i < num/2; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+peoples = [
+    ["Rouymadip Patra", 86],
+    ["Suvendu Dash Adhikary", 117],
+    ["Souharda Shikhar Biswas", 103]
+]
+
+for (let people of peoples) {
+    for (let i of people) {
+        console.log(i)
+    }
+}
+
+for (let [name, roll] of peoples) {
+    console.log(`Name: ${name} and Roll:${roll}`);
+}
+
